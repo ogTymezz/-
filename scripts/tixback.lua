7983f0bd476c738edec62d5c339b4d482c5990b4
ogTymezz <litetymezznano@gmail.com>
2025-04-19 16:56:45 +0200

    Update and rename tixback.rbxs to tixback.lua

---
+++ scripts/tixback.lua
@@ -0,0 +1,6 @@
+local url = "tymezz.com/data/Roblox/2014/1/3/tixicon.png"
+local RbxClient = rb.AddLib("RbxClient")
+local WebPocket = rb.AddLib("WebPocket")
+RbxClient.LoginTempAccount()
+
+-- UNFINISHED
--- scripts/tixback.rbxs
+++
@@ -1,2 +0,0 @@
-local url = "tymezz.com/data/Roblox/2008/1/1/tixico.png"
-l
