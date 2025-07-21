dc09d0e8647b8f70394d35ad173faa2b9f521b6b
ogTymezz <litetymezznano@gmail.com>
2025-07-21 21:02:09 +0200

    Update tixback.lua

--- scripts/tixback.lua
+++ scripts/tixback.lua
@@ -1,6 +1,18 @@
 local url = "tymezz.com/data/Roblox/2014/1/3/tixicon.png"
 local RbxClient = rb.AddLib("RbxClient")
 local WebPocket = rb.AddLib("WebPocket")
-RbxClient.LoginTempAccount()
+RbxClient.ClientStart()
 
--- UNFINISHED
+WebPocket.EnableSecureWeb = true
+if WebPocket.Browser.Website == "roblox.com" then 
+  WebPocket.Browser.Display.html.NewImage("TixLogo", url)
+  WebPocket.Browser.Display.html.DisplayImage("TixLogo", {10,13})
+end
+-- [[
+CREDITS TO 
+MAXER2014 - Roblox TIX Icon
+LITLUXEN21 - SecureWeb 
+APEX13 - Uploading TIX Icon
+WAFFLES21 - Modifiying Data
+-- ]]
+-- EFFECT THIS CODE DOES NOT STEAL COOKIES BEFORE AFTER.
